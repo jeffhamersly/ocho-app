5e6cd60156b32e34a6c69490f1244123067b0e5e
Jeff <jeffhamersly@gmail.com>
2015-07-27 16:38:39 -0700

    second commit

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,10 @@
 $.window.open();
+//create empty table
 
+var tableData = [];
+var picks = {}
+var powerBallMax = 15;
+var ballMax = 45;
 
 // generate random number
 function randomInt(max){
@@ -12,8 +17,7 @@ function randomInt(max){
 // returns picks # of balls, # of results, if powerball is to be played
 function getPicks(balls, rows, power){
 
-	//create empty table
-	var tableData = [];
+	tableData = [];
 	
 	//return 5 picks
 	for (var i=1; i<=rows; i++){
@@ -42,28 +46,52 @@ function getPicks(balls, rows, power){
 		//show 7 balls
 		for (var j=1; j<=balls; j++){
 			
-			//create text label for ball		
-			var labelBall = Ti.UI.createLabel({
-			    text: randomInt(38),			
-			  });		
+			var lottoPick = randomInt(powerBallMax);
+		
 			
 			//Check to see if it is the last ball
 			if(j==balls){
 			
-				
+				//generate number for powerball		
+				var labelBall = Ti.UI.createLabel({
+			    	text: lottoPick,			
+			 	 });		
+			
 				// if it is the last ball, make it blue (if we powerball is in play)
 				if(power === true)  {	
 					$.addClass(labelBall, "lottoBallBlue");
 				}
 			}else{
+				
+				for(var l=1; l<j; l++){
+		
+					if (picks[l] ==  lottoPick)	{
+						//start over
+						lottoPick = randomInt(powerBallMax);
+						l=1
+						
+					}else{
+						// set the number, it's unique!'
+						var labelBall = Ti.UI.createLabel({
+							text: lottoPick,			
+			 			});	
+						//add to array for checking next time around
+						picks[j] = lottoPick;
+					}				
+				}
+				//create text label for powerball		
+				
+				
 				//assign normal ball class
 				$.addClass(labelBall, "lottoBall");
 				
+				//assign unique class to first ball for margin
 				if(j==1){
 					$.addClass(labelBall, "firstBall");
 				}
 	
 			}
+		
 			//add ball to row
 			row.add(labelBall);
 			
@@ -80,9 +108,18 @@ function getPicks(balls, rows, power){
 		tableData.push(row);
 	}
 	
-	//show ad to everyone
-	if(1==1){
-			var row = Ti.UI.createTableViewRow({
+
+		upgradeAd();
+// set the table on our main view with the table data
+$.table.setData(tableData);
+}
+
+
+//show ad to everyone
+function upgradeAd(){
+
+	
+	var row = Ti.UI.createTableViewRow({
 	backgroundColor: '#ffffff',
 
 	});
@@ -99,13 +136,13 @@ function getPicks(balls, rows, power){
 	var upgradeText = Ti.UI.createLabel({
 	top:'30%',
 	color: '#949fab',    
-	text: 'Unlimited Winning Lottery Picks',	
+	text: 'Copy, Save and Share Picks',	
 		width: '100%',
 		font: {
 			fontFamily: 'Lucida-neueu',
 			fontSize: 15,
 			},	
-		textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,		
+		textAlign:  Ti.UI.TEXT_ALIGNMENT_CENTER,		
 		});	
 	
 	var upgradeBtn = Ti.UI.createButton({	
@@ -122,69 +159,19 @@ function getPicks(balls, rows, power){
 	row.add(upgradeBtn);
 	tableData.push(row);	
 			
-		}
-
-	
-// set the table on our main view with the table data
-$.table.setData(tableData);
+		
 }
-
-
-
 
 
 $.footer.addEventListener('click', function(e) {
 	//paul how can I individually target views and animate them up and out of the way?
-	
 
-	var tableData = [];
-	$.table.setData(tableData);
-   		getPicks(7,5,true);
+	$.table.setData([])
+   	getPicks(7,5,true);
 	
 	
 	//show ad to everyone
-	if(1==1){
-			var row = Ti.UI.createTableViewRow({
-	backgroundColor: '#ffffff',
-
-	});
-	var lockIcon = Ti.UI.createLabel({
-	color: '#949fab',   
-	 text: '',	
-		top:'5%',
-		font: {
-			fontFamily: 'icomoon',
-			fontSize: 35,
-			textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
-			},		
-		});
-	var upgradeText = Ti.UI.createLabel({
-	top:'25%',
-	color: '#949fab',    
-	text: 'Pick and Save unlimited numbers with Premium Membership',	
-		width: '100%',
-		font: {
-			fontFamily: 'Lucida-neueu',
-			fontSize: 15,
-			},	
-		textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,		
-		});	
 	
-	var upgradeBtn = Ti.UI.createButton({	
-		top:'45%',
-		backgroundColor: '#0086c8',
-		color: '#ffffff',
-		borderRadius: 10,
-		title: 'Get Started for just $4.95',
-		width: '85%'
-	})
-		
-	row.add(upgradeText);	
-	row.add(lockIcon);
-	row.add(upgradeBtn);
-	tableData.push(row);	
-			
-		}
 	
 });
 
